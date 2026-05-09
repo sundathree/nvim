b73012951a43bd28829082efb8d9020232ec3225
Miguel Carvalho <dev@sundathr.ee>
2026-05-09 19:43:11 -0300

    lualine update

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -43,21 +43,34 @@ return {
   },
   {
     "nvim-lualine/lualine.nvim",
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
       options = {
-        theme = "auto",
-        section_separators = "",
-        component_separators = "|",
         icons_enabled = false,
+        theme = 'auto',
+        component_separators = "|",
+        section_separators = "",
       },
       sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "filename" },
-        lualine_c = {},
-        lualine_x = { "filetype", "encoding" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
+        lualine_a = {'mode'},
+        lualine_b = {'branch'},
+        lualine_c = {'filename'},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
       },
-    },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {}
+    }
   },
 }
