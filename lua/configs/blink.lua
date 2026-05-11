28af131de08060b1cdb2014d11f9e962581400b8
Miguel Carvalho <dev@sundathr.ee>
2026-05-11 12:20:52 -0300

    config cleanup

---
+++ lua/configs/blink.lua
@@ -0,0 +1,29 @@
+return {
+  keymap = {
+    preset = "default",
+    ["<CR>"] = { "accept", "fallback" },
+    ["<C-b>"] = { "show", "show_documentation", "hide_documentation" },
+    ["<C-j>"] = { "select_next", "fallback" },
+    ["<C-k>"] = { "select_prev", "fallback" },
+    ["<Esc>"] = { function(cmp) cmp.hide() end, "fallback" },
+  },
+
+  appearance = {
+    nerd_font_variant = "normal",
+  },
+
+  sources = {
+    default = { "lsp", "path", "snippets", "buffer" },
+  },
+
+  completion = {
+    trigger = {
+      show_on_keyword = true,
+    },
+
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 500
+    },
+  }
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,27 +18,7 @@ return {
   {
     "saghen/blink.cmp",
     version = "*",
-    opts = {
-      keymap = {
-        preset = "default",
-        ["<CR>"] = { "accept", "fallback" },
-        ["<C-b>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<C-j>"] = { "select_next", "fallback" },
-        ["<C-k>"] = { "select_prev", "fallback" },
-        ["<Esc>"] = { function(cmp) cmp.hide() end, "fallback" },
-      },
-      appearance = {
-        nerd_font_variant = "propo",
-      },
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-      },
-      completion = {
-        trigger = {
-          show_on_keyword = true,
-        },
-      },
-    },
+    opts = require("configs.blink")
   },
   { "b0o/schemastore.nvim" }
 }
