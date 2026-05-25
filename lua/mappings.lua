145ad8575cb3e55e1ed21226f621ccfded954afa
Miguel Carvalho <dev@sundathr.ee>
2026-05-25 01:36:05 -0300

    style: removed vim variables

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,15 +1,17 @@
 vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>")
-vim.keymap.set("n", "<C-s>", "<cmd>w<CR>")
+vim.keymap.set("n", "<leader>w", "<cmd>update<CR>")
 vim.keymap.set("n", "<leader>x", "<cmd>bd<CR>")
+vim.keymap.set("n", "<leader>q", "<cmd>q<CR>")
+vim.keymap.set("n", "<leader>o", "<cmd>so<CR>")
 vim.keymap.set({"n", "v"}, "x", '"_x')
 
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', { silent = true })
 vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', { silent = true })
 
 vim.keymap.set("n", ",,", function()
-	if vim.bo.filetype == "netrw" then
-		vim.cmd("bd")
-	else
-		vim.cmd("Ex")
-	end
+    if vim.bo.filetype == "netrw" then
+	vim.cmd("bd")
+    else
+	vim.cmd("Ex")
+    end
 end, { silent = true })
