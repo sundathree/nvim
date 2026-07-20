57ec42ba0aecc3942f9f393963018e952b24312c
Miguel Carvalho <dev@sundathr.ee>
2026-07-20 01:06:16 -0300

    chore: sync

--- lua/plugins/monochrome.lua
+++ lua/plugins/monochrome.lua
@@ -2,8 +2,8 @@
     {
         "https://github.com/chriskempson/base16-vim",
         config = function()
-            vim.o.background = "light"
-            vim.cmd("colorscheme base16-grayscale-light")
+            vim.o.background = "dark"
+            vim.cmd("colorscheme base16-grayscale-dark")
         end
     }
  }
